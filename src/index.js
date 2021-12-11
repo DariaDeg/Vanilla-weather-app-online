30ff4dc7a7a97959e9b52f4a735ad4d6bc5b516e
DariaDeg <daria.degtyarenko@exactprosystems.com>
2021-12-12 01:22:02 +0300

    City variable added

--- src/index.js
+++ src/index.js
@@ -22,13 +22,13 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descrElement = document.querySelector("#descr");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descrElement.innerHTML = response.data.weather[0].description;
@@ -38,6 +38,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "b36cc77b9c4e96dfd09d7b186068f93d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
