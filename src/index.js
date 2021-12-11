d4f0562b0a2a8b2ad2db1da13cfc7fdab30c74c7
DariaDeg <daria.degtyarenko@exactprosystems.com>
2021-12-12 00:53:02 +0300

    JS Weather Description

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descrElement = document.querySelector("#descr");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descrElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "b36cc77b9c4e96dfd09d7b186068f93d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
