97631c9abed50e89c2c14b632a41a43c213bd066
DariaDeg <daria.degtyarenko@exactprosystems.com>
2021-12-19 00:26:08 +0300

    Li capitalisation fixed, spaces added

--- src/index.js
+++ src/index.js
@@ -63,7 +63,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "b36cc77b9c4e96dfd09d7b186068f93d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
